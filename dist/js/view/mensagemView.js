89b03ddcd37624ce187686c4e7f2381feba7d2e4
Everton Rocha <imevertonrch@gmail.com>
2025-03-18 01:14:17 -0300

    removendo arquivos transpilados

--- dist/js/view/mensagemView.js
+++
@@ -1,8 +0,0 @@
-import View from "./view.js";
-export default class MensagemView extends View {
-    template(msg) {
-        return `
-            <p class="alert alert-success col-6 offset-3">${msg}</p>
-        `;
-    }
-}
